718407625d61dfcaa07d3ec05d6bbc538c4da939
Riad <of.rid@mail.ru>
2024-08-06 13:04:42 +0300

    Initial commit

---
+++ include/Rectangle.h
@@ -0,0 +1,12 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+#include "Quadrangle.h"
+
+class Rectangle : public Quadrangle
+{
+public:
+    Rectangle(double a, double b);
+};
+
+#endif // RECTANGLE_H
